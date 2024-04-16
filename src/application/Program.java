2d6c1eafab23487d8bc0e13aa4d5b1776303c5ba
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-04-16 13:14:13 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,11 @@ public class Program {
         Seller newseller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newseller);
         System.out.println("Inserted! New id = " + newseller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ==== ");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
